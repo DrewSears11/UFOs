3cfc6fc4294b4e739b0ddbab6f9ac99358ef0c3e
Drewski1986 <108240844+Drewski1986@users.noreply.github.com>
2022-09-14 21:18:39 -0400

    Module 11 assignment update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # UFOs
 Module 11
+
+###1. Overview of Project: The purpose of this project was to create a webpage where UFO enthusiasts can easily idenitfy 
+UFO sitings using a search feature on a HTML page.
+
+###2. Results: By scrolling to the bottom of the webpage you are able to search by 5 different variables.  You can find 
+results of the date it was sited, city, state, country and even shape of the UFO by entering which variable you would like
+to search in results page.  I tested to see if Richmond, VA has been a hot spot for UFOs and you can see it is not.
+
+###3. Summary:  One drawback of the webpage is ----.  A couple recomendations I would have to enhance this would be 1, 2. 
--- static/js/app.js
+++ static/js/app.js
@@ -29,34 +29,20 @@ var filters = {}
 
 
 // 3. Use this function to update the filters. 
-function updateFilters() 
-{ let date = d3.select("#datetime").property("value");
-  let city = d3.select("#city").property("value");
-  let state = d3.select("#state").property("value");
-  let country = d3.select("#country").property("value");
-  let shape = d3.select("#shape").property("value");
+function updateFilters() { 
 
-  let filteredData = tableData;
-
- // Check to see if a date was entered and filter the
-// data using that date.
-if (date) {
-  // Apply `filter` to the table data to only keep the
-  // rows where the `datetime` value matches the filter value
-  filteredData = filteredData.filter(row => row.datetime === date);
-}
 
     // 4a. Save the element that was changed as a variable.
-    d3.selectAll("#filter-btn").on("click", updateFilters);
+    let changedElement = d3.select(this);
     // 4b. Save the value that was changed as a variable.
-    filtervalue = d3.selectAll("#filter-btn").on("click", updateFilters);
+    let elementValue = changedElement.proper
     // 4c. Save the id of the filter that was changed as a variable.
-    let filterID = filtervalue
+    let filterID = changedElement.attr("id")
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
-  if (date) {
-    filters[filterID] = date
+    if (elementValue) {
+    filters[filterID] = elementValue;
   }
   else {
     delete filters[filterID]
@@ -77,13 +63,11 @@ if (date) {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    if (date) {
-      filteredData = filteredData.filter(row => row.datetime === date);
-    };
-  
-      
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
